d10bc9ce479aa56f7abc553713704d70ad4c42f4
fglowicki <f.glowicki@p-programisci.pl>
2024-07-18 09:13:48 +0200

    feat: Add React into the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
-
 import node from "@astrojs/node";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: node({
-    mode: "standalone",
+    mode: "standalone"
   }),
+  integrations: [react()]
 });
